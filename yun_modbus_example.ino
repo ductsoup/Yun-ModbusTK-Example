86ac1573235d71648dc20a42fd4c20858934a4de
Craig <ductsoup@users.noreply.github.com>
2015-03-22 07:30:05 -0400

    Create yun_modbus_example.ino
    Initial

---
+++ yun_modbus_example.ino
@@ -0,0 +1,31 @@
+
+#include <Bridge.h>
+
+Process p;
+#define BUF_SIZE 32
+char buf[BUF_SIZE];
+
+void setup() {
+  Serial.begin(115200);
+  // Start the bridge
+  Bridge.begin();
+  // Start the modbus server
+  p.begin("python");
+  p.addParameter("/mnt/sd/modbus_tcp_slave.py"); // modbus float holding registers
+  p.addParameter("40001");                       // read only start address
+  p.addParameter("6");                           // read only number of holding registers
+  p.addParameter("40101");                       // read/write start address (optional)
+  p.addParameter("6");                           // read/write number of holding registers (optional)
+  p.runAsynchronously();
+}
+void loop() {
+  // AVR -> WRT (modbus read only), write some values to the modbus server
+  Bridge.put("40001", String(PI));
+  Bridge.put("40003", String(millis()));
+  Bridge.put("40005", String(5280));
+
+  // AVR <- WRT (modbus read/write), read some values from the modbus server
+  Bridge.get("40101", buf, BUF_SIZE);
+  Serial.println(atof(buf));
+  delay(500);
+}
